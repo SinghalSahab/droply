54e903771db20d50a1a06c58694d7181b33a2b3b
SinghalSahab <prakharsinghal10a@gmail.com>
2025-03-14 23:44:02 +0530

    complete

--- components/ActionDropdown.tsx
+++ components/ActionDropdown.tsx
@@ -3,7 +3,14 @@ import { Models } from 'node-appwrite'
 import React, { useState } from 'react'
 import {
     Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
   } from "@/components/ui/dialog"
+  
   import {
     DropdownMenu,
     DropdownMenuContent,
@@ -16,13 +23,125 @@ import Image from 'next/image'
 import { actionsDropdownItems } from '@/constants'
 import Link from 'next/link'
 import { constructDownloadUrl } from '@/lib/utils'
+import { Input } from './ui/input'
+import { Button } from './ui/button'
+import { deleteFile, renameFile, updateFileUsers } from '@/lib/actions/file.action'
+import { usePathname } from 'next/navigation'
+import { FileDetails, ShareInput } from './ActionsModalContent'
   
 
   
 const ActionDropdown = ({file}:{file:Models.Document}) => {
     const [isModalOpen,setIsModalOpen] = useState(false)
     const [isDropdownOpen,setDropdownOpen] = useState(false)
+     const [name, setName] = useState(file.name)
+     const [isLoading,setIsLoading] = useState(false)
+     const path = usePathname();
+     const [emails, setEmails] = useState<string[]>([]);
     const [action,setAction] = useState<ActionType| null >(null)
+    const closeAllModals = () => {
+        setIsModalOpen(false)
+        setDropdownOpen(false)
+        setName(file.name)
+        setAction(null)
+
+    }
+
+    const handleAction = async() => {
+       if(!action) return;
+
+       setIsLoading(true)
+
+       let success = false;
+
+       const actions =  {
+         rename: async () => {
+               await renameFile({fileId: file.$id, name, extension: file.extension, path})
+               success = true
+           },
+           delete: async () => {
+               await deleteFile({ fileId: file.$id, bucketFileId: file.bucketFileId, path })
+               success = true
+           },
+           share: async () => {
+               updateFileUsers({fileId:file.$id,emails,path})
+           }
+       }
+
+       success = await actions[action.value as keyof typeof actions]();
+       if(success) {
+        closeAllModals();
+        } 
+        setIsLoading(false)
+   
+    }
+
+    const handleRemoveUser = async (email: string) => {
+        const updatedEmails = emails.filter((e) => e !== email);
+
+        const success = await updateFileUsers({
+          fileId: file.$id,
+          emails: updatedEmails,
+          path,
+        });
+    
+        if (success) setEmails(updatedEmails);
+        closeAllModals();
+      };
+    const renderDialogContent = () =>{
+        if(!action) return null
+
+        const {value,label} = action
+        return (
+            <DialogContent  className='shad-dialog button'>
+                <DialogHeader className='flex flex-col gap-3'>
+                    <DialogTitle className="text-center text-light-100">{label}</DialogTitle>
+                    {value === "rename" && (
+                        <Input
+                          type="text"
+                          value={name}
+                          onChange={(e) => setName(e.target.value)}
+                        />
+          )}
+          {value === "details" && <FileDetails file={file} />}
+          {value === "share" && (
+            <ShareInput
+              file={file}
+              onInputChange={setEmails}
+              onRemove={handleRemoveUser}
+            />
+          )}
+           {value === "delete" && (
+            <p className="delete-confirmation">
+              Are you sure you want to delete{` `}
+              <span className="delete-file-name">{file.name}</span>?
+            </p>
+          )}
+               </DialogHeader>
+               {['rename','delete','share'].includes(value) && (
+                <DialogFooter className='flex flex-col gap-3 md:flex-row'>
+                    <Button onClick={closeAllModals} className='modal-cancel-button'>
+                        Cancel
+                    </Button>
+
+                    <Button onClick={handleAction} className='modal-submit-button'>
+                        <p className='capitalize'>{value}</p>
+                        {isLoading && (
+                            <Image
+                            src="/assets/icons/loader.svg"
+                            alt="loader"
+                            width={24}
+                            height={24}
+                            className="animate-spin"
+                          />
+                        )}
+                    </Button>
+
+                </DialogFooter>
+               )}
+            </DialogContent>
+        )
+    }
   return (
         <Dialog open={isModalOpen} onOpenChange={setIsModalOpen}>
         <DropdownMenu open={isDropdownOpen} onOpenChange={setDropdownOpen}>
@@ -75,7 +194,7 @@ const ActionDropdown = ({file}:{file:Models.Document}) => {
     ))}
   </DropdownMenuContent>
 </DropdownMenu>
-
+   {renderDialogContent()}
 </Dialog>
 
   )
